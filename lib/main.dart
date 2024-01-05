384be7e8a55d81b3f94ce81e093b5aa642cbfa77
Thibaud-LAMON <thib.lamon@gmail.com>
2024-01-05 11:41:45 +0100

    exercice 2

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Nouveau texte'),
+      home: const MyHomePage(title: 'Appbar'),
     );
   }
 }
@@ -33,10 +32,13 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      /*appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
         title: Text(widget.title),
-      ),*/
+        actions: <Widget>[
+          IconButton(icon: const Icon(Icons.add), onPressed: (){},)
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -45,7 +47,6 @@ class _MyHomePageState extends State<MyHomePage> {
               color: Colors.red,
               width: double.infinity,
               height: 100,
-              child: const Text('100'),
             ),
             Expanded(
               child: Container(
@@ -57,7 +58,6 @@ class _MyHomePageState extends State<MyHomePage> {
               color: Colors.green,
               width: double.infinity,
               height: 100,
-              child: const Text('100'),
             ),
           ],
         ),
