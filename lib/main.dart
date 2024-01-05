ffdfe3185fd6db98891ebd26494a4693eaebc8bf
Thibaud-LAMON <thib.lamon@gmail.com>
2024-01-05 11:28:47 +0100

    exercice 1

--- lib/main.dart
+++ lib/main.dart
@@ -29,48 +29,38 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      /*appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
-      ),
+      ),*/
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+              color: Colors.red,
+              width: double.infinity,
+              height: 100,
+              child: const Text('100'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Expanded(
+              child: Container(
+                color: Colors.blue,
+                width: double.infinity,
+              ),
             ),
-            const Row(mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Text 1'),
-                SizedBox(width: 100),
-                Text('Text 2'),
-                SizedBox(width: 100),
-                Text('Text 3'),
-              ],
-            )
+            Container(
+              color: Colors.green,
+              width: double.infinity,
+              height: 100,
+              child: const Text('100'),
+            ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
